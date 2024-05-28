b04324f970422e8817e1b5226c48cbb8d4f7f765
noblet <appiahpatricknoblet@gmail.com>
2024-05-28 17:56:04 +0100

    Added a check to the status of the API

---
+++ api/v1/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" App routing module """
+
+import os
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_session(self):
+    """
+    Remove the current SQLAlchemy Session after each request.
+
+    This function is registered to be called after each request to ensure
+    that the SQLAlchemy Session is properly closed,
+    preventing any potential resource leaks.
+    """
+    storage.close()
+
+
+
+if __name__ == "__main__":
+    app.run(
+            host=os.getenv("HBNB_API_HOST", default="0.0.0.0"),
+            port=int(os.getenv("HBNB_API_PORT", default="5000")),
+            threaded=True
+    )
---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" API endpoints handler module """
+
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route("/status")
+def get_status():
+    """Returns the status of the API service."""
+    data = {"status": "OK"}
+    return jsonify(data)
+
