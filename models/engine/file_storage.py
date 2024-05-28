0b56874d0ae55925d257e651bde500d03d0c5288
noblet <appiahpatricknoblet@gmail.com>
2024-05-28 16:32:52 +0100

    added new bug fixes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,7 +63,7 @@ class FileStorage:
         if cls_name is None or id is None:
             return None
 
-        key = f"{cls.__name__}.{id}"
+        key = f"{cls_name.__name__}.{id}"
         return self.__objects.get(key)
 
     def count(self, cls_name=None):
