568c54d7ea63183f98dfe66c7affa0337cc4fb90
noblet <appiahpatricknoblet@gmail.com>
2024-05-28 18:15:41 +0100

    Added a check to the status of the API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,9 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route("/status")
 def get_status():
     """Returns the status of the API service."""
     data = {"status": "OK"}
     return jsonify(data)
-
